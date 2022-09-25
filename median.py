0ed0fce4181af20e18291e5c435f4ecf0a413478
Mihaela Peneva <mihaela.peneva@kcl.ac.uk>
2022-09-25 18:19:09 +0300

    Add solution

--- median.py
+++ median.py
@@ -9,4 +9,10 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+if len(numbers) % 2 != 0:
+    median = numbers[len(numbers) // 2]
+else:
+    median = (numbers[len(numbers) // 2] + numbers[len(numbers) // 2 - 1]) / 2
+    
+print(median)
